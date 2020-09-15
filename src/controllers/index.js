687d8ee74e4f688b689e06638ab5b0c00b23b6ca
DaJuan Harris <dajuancancode@gmail.com>
2020-09-15 15:08:34 -0400

    Added read routes using express

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,11 +5,52 @@ const dbFile = path.resolve(__dirname, "../db/notes.json");
 
 const createNote = (req, res) => {
   const notes = loadNotes(dbFile);
+  let id = notes.length;
+  let body = req.body;
 
-  let note = JSON.stringify(req.body);
+  let note = { ...body, id };
   notes.push(note);
 
-  saveNotes(notes, dbFile);
+  try {
+    saveNotes(notes, dbFile);
+    res.status(200);
+    res.json(note);
+  } catch (e) {
+    const message = { error: "Couldn't add note", errorMessage: e.message };
+    res.status(400);
+    res.json(message);
+  }
 };
 
-module.exports = { createNote };
+const readNote = (req, res) => {
+  const notes = loadNotes(dbFile);
+  let { id } = req.params;
+
+  id = Number(id);
+
+  const note = notes.find((note) => note.id === id);
+
+  if (note !== undefined) {
+    res.status(200);
+    res.json(JSON.stringify(note));
+  } else {
+    const message = { error: "No such note found" };
+    res.status(400);
+    res.json(message);
+  }
+};
+
+const readNotes = (req, res) => {
+  const notes = loadNotes(dbFile);
+
+  if (notes.length > 0) {
+    res.status(200);
+    res.json(notes);
+  } else {
+    const message = { error: "Notes array empty" };
+    res.status(400);
+    res.json(message);
+  }
+};
+
+module.exports = { createNote, readNote, readNotes };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 
-const { createNote } = require("../controllers/index");
+const { createNote, readNote, readNotes } = require("../controllers/index");
 
 const apiRouter = express.Router();
 
 apiRouter.post("/", (req, res) => {
-  try {
-    createNote(req, res);
-    res.status(200);
-    res.json(req.body);
-  } catch (e) {
-    const message = { error: "Couldn't add note", errorMessage: e.message };
-    res.status(400);
-    res.json(message);
-  }
+  createNote(req, res);
+});
+
+apiRouter.get("/:id", (req, res) => {
+  readNote(req, res);
+});
+
+apiRouter.get("/", (req, res) => {
+  readNotes(req, res);
 });
 
 module.exports = apiRouter;
